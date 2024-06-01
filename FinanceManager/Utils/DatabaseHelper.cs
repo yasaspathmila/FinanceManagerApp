3b3ec79515ecc991ba5df1c23605addbeec99dae
yasaspathmila <pathmilakky.21@uom.lk>
2024-06-01 19:21:45 +0530

    connecting database

---
+++ FinanceManager/Utils/DatabaseHelper.cs
@@ -0,0 +1,22 @@
+﻿using MongoDB.Driver;
+
+namespace PersonalFinanceManager.Utils
+{
+    public static class DatabaseHelper
+    {
+        private static readonly IMongoClient _client;
+        private static readonly IMongoDatabase _database;
+
+        static DatabaseHelper()
+        {
+            var connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["MongoDbConnectionString"].ConnectionString;
+            _client = new MongoClient(connectionString);
+            _database = _client.GetDatabase("PersonalFinanceManagerDB");
+        }
+
+        public static IMongoCollection<T> GetCollection<T>(string name)
+        {
+            return _database.GetCollection<T>(name);
+        }
+    }
+}
